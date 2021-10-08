787191fdae8f9effc04624fc8138693404b0cfc1
yku <yannic.kuester@gmail.com>
2021-10-08 11:29:13 +0200

    #6: Integrate Fremarker as template engine
    Added code to create all non-existing folders along the output path for the generated report
    Code refactoring according to Kotlin idioms
    Added Dokka plugin to generate project documentation
    Improved Documentation
    Removed no longer required HelloWorld class
    Added FreeMarkerService
    Created BpmnObject data class
    Added basic template for testing purposes
    Added Freemarker dependency

---
+++ src/main/kotlin/FreeMarkerService.kt
@@ -0,0 +1,45 @@
+import freemarker.template.Configuration
+import freemarker.template.TemplateExceptionHandler
+import models.BpmnObject
+import java.io.File
+import java.io.FileWriter
+import java.nio.file.FileAlreadyExistsException
+import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+
+/**
+ * A simple singleton service to create an output HTML file from a given input template and [models.BpmnObject]
+ */
+object FreeMarkerService {
+    private val cfg = Configuration(Configuration.VERSION_2_3_31)
+
+    init {
+        cfg.apply {
+            setClassForTemplateLoading(FreeMarkerService::class.java, "templates")
+            defaultEncoding = "UTF-8"
+            locale = Locale.GERMAN
+            templateExceptionHandler = TemplateExceptionHandler.RETHROW_HANDLER
+        }
+    }
+
+    /**
+     * Creates an output html file from a given template file fills it with the [input] data.
+     * @param input input data.
+     * @param templateName name of the template file to be used, will be prefixed with the default path "templates/".
+     * @param outputPath path to the output file to be created by this method, defaults to "out/[templateName].html".
+     */
+    fun writeTemplate(input: Map<String, BpmnObject>, templateName: String, outputPath: String = "out/$templateName.html") {
+        val filePath = Path(outputPath)
+        try {
+            filePath.parent.createDirectories()
+        }catch (e: FileAlreadyExistsException) {
+        /* This method throws an exception if a directory along the path already exists, but generates missing directories anyways.
+         * Thus, the exception can be ignored unless the information about already existing directories would be required.
+         */
+        }
+        FileWriter(filePath.toFile()).use {
+            cfg.getTemplate(templateName).process(input, it)
+        }
+    }
+}
