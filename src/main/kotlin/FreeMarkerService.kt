24d1ea6a927db9099cc056deb21e4ff2ad4328c6
Jens-Hilmar Bradt <jens-hilmar.bradt@novatec-gmbh.de>
2021-10-29 17:34:27 +0200

    #6: Fixed Spotless Issues

--- src/main/kotlin/FreeMarkerService.kt
+++ src/main/kotlin/FreeMarkerService.kt
@@ -1,10 +1,9 @@
 import freemarker.template.Configuration
 import freemarker.template.TemplateExceptionHandler
 import models.BpmnObject
-import java.io.File
 import java.io.FileWriter
 import java.nio.file.FileAlreadyExistsException
-import java.util.*
+import java.util.Locale
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
 
@@ -29,14 +28,18 @@ object FreeMarkerService {
      * @param templateName name of the template file to be used, will be prefixed with the default path "templates/".
      * @param outputPath path to the output file to be created by this method, defaults to "out/[templateName].html".
      */
-    fun writeTemplate(input: Map<String, BpmnObject>, templateName: String, outputPath: String = "out/$templateName.html") {
+    fun writeTemplate(
+        input: Map<String, BpmnObject>,
+        templateName: String,
+        outputPath: String = "out/$templateName.html"
+    ) {
         val filePath = Path(outputPath)
         try {
             filePath.parent.createDirectories()
-        }catch (e: FileAlreadyExistsException) {
-        /* This method throws an exception if a directory along the path already exists, but generates missing directories anyways.
-         * Thus, the exception can be ignored unless the information about already existing directories would be required.
-         */
+        } catch (e: FileAlreadyExistsException) {
+            /* This method throws an exception if a directory along the path already exists, but generates missing directories anyways.
+             * Thus, the exception can be ignored unless the information about already existing directories would be required.
+             */
         }
         FileWriter(filePath.toFile()).use {
             cfg.getTemplate(templateName).process(input, it)
