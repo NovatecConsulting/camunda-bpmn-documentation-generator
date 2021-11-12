01051c58e66e5e778fb0cecfc5d37375d9acb0d9
Konstantin Wiens <konstantin.wiens@novatec-gmbh.de>
2021-11-12 13:44:50 +0100

    #14: Refactorings in GenerateMojo

--- src/main/kotlin/info/novatec/cbdg/plugin/GenerateMojo.kt
+++ src/main/kotlin/info/novatec/cbdg/plugin/GenerateMojo.kt
@@ -5,24 +5,25 @@ import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import java.io.File
+import java.io.FileNotFoundException
 
 @Mojo(name = "generate")
 class GenerateMojo : AbstractMojo {
     constructor() : super()
 
     @Parameter(property = "camundaBpmnDir", defaultValue = "\${project.basedir}/src/main/resources/bpmn")
-    val camundaBpmnDir = File("\${project.basedir}/src/main/resources/bpmn")
+    lateinit var camundaBpmnDir: File
 
-    @Parameter(property = "resultOutputDir", defaultValue = "\${project.build.directory}/classes/html")
-    val resultOutputDir = File("\${project.build.directory}/classes/html")
+    @Parameter(property = "resultOutputDir", defaultValue = "\${project.build.directory}/cbdg/html")
+    lateinit var resultOutputDir: File
 
     override fun execute() {
-        camundaBpmnDir.listFiles().forEach { bpmnFile ->
-            log.info(bpmnFile.absolutePath)
-            FileUtils.writeStringToFile(File(resultOutputDir.absolutePath, bpmnFile.name + ".html"), "<html>" + bpmnFile.absolutePath + "</html>", "UTF-8")
-        }
-        resultOutputDir.listFiles().forEach { file ->
-            log.info(file.absolutePath)
-        }
+        camundaBpmnDir.listFiles()?.forEach {
+            log.info(it.absolutePath)
+            FileUtils.writeStringToFile(File(resultOutputDir.absolutePath, it.name + ".html"), "<html>" + it.absolutePath + "</html>", "UTF-8")
+        } ?: throw FileNotFoundException("${camundaBpmnDir.absolutePath} don't exist.")
+        resultOutputDir.listFiles()?.forEach {
+            log.info(it.absolutePath)
+        } ?: throw FileNotFoundException("${resultOutputDir.absolutePath} don't exist.")
     }
 }
