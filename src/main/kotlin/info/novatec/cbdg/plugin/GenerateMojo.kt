8d7b42f626fe8ec944c44438ef9d10cd246666a8
Konstantin Wiens <konstantin.wiens@novatec-gmbh.de>
2021-12-03 15:39:47 +0100

    #19: The plugin provides the default template file

--- src/main/kotlin/info/novatec/cbdg/plugin/GenerateMojo.kt
+++ src/main/kotlin/info/novatec/cbdg/plugin/GenerateMojo.kt
@@ -7,11 +7,17 @@ import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.FileOutputStream
 
 @Mojo(name = "generate")
 class GenerateMojo : AbstractMojo() {
 
-    @Parameter(property = "templateFile", defaultValue = "\${project.basedir}/src/main/resources/templates/basic.ftl")
+    /**
+     * Default usage is the templates/default.ftl from Jar-File.
+     * To use it, it will be create in Build-Dir of the target project the empty file default.ftl
+     * and fill it with the stream of templates/default.ftl from Jar-File
+     */
+    @Parameter(property = "templateFile", defaultValue = "\${project.build.directory}/classes/templates/default.ftl")
     lateinit var templateFile: File
 
     @Parameter(property = "camundaBpmnDir", defaultValue = "\${project.basedir}/src/main/resources/bpmn")
@@ -21,6 +27,10 @@ class GenerateMojo : AbstractMojo() {
     lateinit var resultOutputDir: File
 
     override fun execute() {
+        if (templateFile.name.equals("default.ftl")) {
+            FileOutputStream(templateFile, false).use { it -> javaClass.classLoader.getResourceAsStream("templates/default.ftl").transferTo(it) }
+        }
+
         camundaBpmnDir.listFiles()?.forEach {
             log.info("Generating documentation for file ${it.absolutePath}")
             log.info("Using template ${templateFile.absolutePath}")
