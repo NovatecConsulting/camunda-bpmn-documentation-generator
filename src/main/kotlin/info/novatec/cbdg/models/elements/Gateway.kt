b1a227d3b9b78beea4383f3b9fbccb81189d7808
Konstantin Wiens <konstantin.wiens@novatec-gmbh.de>
2022-01-28 15:17:33 +0100

    #29 Spotless-Plugin fails if Kotlin files have lines with more than 160 characters

--- src/main/kotlin/info/novatec/cbdg/models/elements/Gateway.kt
+++ src/main/kotlin/info/novatec/cbdg/models/elements/Gateway.kt
@@ -2,4 +2,5 @@ package info.novatec.cbdg.models.elements
 
 import info.novatec.cbdg.models.enums.GatewayTypeEnum
 
-class Gateway(id: String, name: String, version: String, documentation: String? = null, type: GatewayTypeEnum? = null) : BpmnElement(id, name, version, documentation)
+class Gateway(id: String, name: String, version: String, documentation: String? = null, type: GatewayTypeEnum? = null) :
+    BpmnElement(id, name, version, documentation)
--- src/main/kotlin/info/novatec/cbdg/models/elements/StartEvent.kt
+++ src/main/kotlin/info/novatec/cbdg/models/elements/StartEvent.kt
@@ -2,4 +2,5 @@ package info.novatec.cbdg.models.elements
 
 import info.novatec.cbdg.models.enums.StartEventEnum
 
-class StartEvent(id: String, name: String, version: String, documentation: String? = null, val type: StartEventEnum? = null) : BpmnElement(id, name, version, documentation)
+class StartEvent(id: String, name: String, version: String, documentation: String? = null, val type: StartEventEnum? = null) :
+    BpmnElement(id, name, version, documentation)
