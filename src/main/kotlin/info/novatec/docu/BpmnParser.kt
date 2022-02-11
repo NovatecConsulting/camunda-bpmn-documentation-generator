13d74ec23dff4aa2dbb4a51c4f0404f821b5af39
yku <yannic.kuester@gmail.com>
2022-02-11 09:50:11 +0100

    #28: Call BpmnParser from GenerateMojo
    closing #28

--- src/main/kotlin/info/novatec/docu/BpmnParser.kt
+++ src/main/kotlin/info/novatec/docu/BpmnParser.kt
@@ -8,6 +8,13 @@ import java.io.File
 
 object BpmnParser {
 
+    /**
+     * Parses a bpmn file into a BpmnObject.
+     *
+     * @param bpmnFile the BPMN input file that should be parsed.
+     * @param bpmnImagePath path to a manually generated image of the BPMN diagram that will be included in the generated output.
+     * Viable formats: png, svg.
+     */
     fun parseBpmnFile(bpmnFile: File, bpmnImagePath: String): BpmnObject {
         val bpmnModelInstance = Bpmn.readModelFromFile(bpmnFile)
         val model = bpmnModelInstance.model
